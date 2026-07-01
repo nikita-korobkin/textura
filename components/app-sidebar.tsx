567f7edf4911f12044a7266e8b9e957201c4a01f
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-07-01 20:05:38 +0300

    Add logo to sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,5 +1,18 @@
-import { Sidebar } from '@/components/ui/sidebar';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+} from '@/components/ui/sidebar';
+import { Logo } from '@/components/logo';
 
 export function AppSidebar() {
-  return <Sidebar />;
+  return (
+    <Sidebar>
+      <SidebarHeader className="h-13 pl-4">
+        <Logo variant="nav" />
+      </SidebarHeader>
+
+      <SidebarContent />
+    </Sidebar>
+  );
 }
