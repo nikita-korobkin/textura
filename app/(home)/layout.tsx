6f40e62484bacb2755c0cd18d4dca6c296a15d81
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-07-01 15:19:16 +0300

    Remove navbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,23 +1,11 @@
-'use client';
-
-import { Navbar } from '@/components/navbar/navbar';
-import ThemeSwitcher from '@/components/navbar/theme-switcher';
-
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Navbar>
-        <Navbar.End>
-          <ThemeSwitcher />
-        </Navbar.End>
-      </Navbar>
-      <main className="flex h-dvh items-start justify-center px-4 pt-[25vh]">
-        {children}
-      </main>
-    </>
+    <main className="flex h-dvh items-start justify-center px-4 pt-[25vh]">
+      {children}
+    </main>
   );
 }
--- app/dictionary/layout.tsx
+++ app/dictionary/layout.tsx
@@ -1,25 +1,7 @@
-'use client';
-
-import { Navbar } from '@/components/navbar/navbar';
-import ThemeSwitcher from '@/components/navbar/theme-switcher';
-import { HomeLink } from '@/components/navbar/home-link';
-
 export default function DictionaryLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Navbar className="border-b border-separator bg-background lg:border-b-0 lg:bg-transparent">
-        <Navbar.Start>
-          <HomeLink />
-        </Navbar.Start>
-        <Navbar.End>
-          <ThemeSwitcher />
-        </Navbar.End>
-      </Navbar>
-      <main className="h-dvh overflow-y-auto">{children}</main>
-    </>
-  );
+  return <main className="h-dvh overflow-y-auto">{children}</main>;
 }
